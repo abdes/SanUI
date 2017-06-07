507a059a4921e4fc8082de77eca390f2735220ba
SanUI <SanUI@users.noreply.github.com>
2017-06-07 19:17:01 +0200

    Dummy change

--- Tukui/Libs/oUF_RaidDebuffs/oUF_RaidDebuffs.lua
+++ Tukui/Libs/oUF_RaidDebuffs/oUF_RaidDebuffs.lua
@@ -144,6 +144,8 @@ local Update = function(self, event, unit)
 					rd.index = i
 					rd.type = 'Dispel'
 					rd.filter = filter
+					---dump(rd.filter)
+					---dump(disFilter)
 				end
 			end
 
