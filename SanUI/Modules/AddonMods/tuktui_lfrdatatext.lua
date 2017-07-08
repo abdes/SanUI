508dcd2d3cb406efe43f8caa1495cf85be0cb540
SanUI <SanUI@users.noreply.github.com>
2017-07-08 20:48:56 +0200

    Fix lfr datatext

--- SanUI/Modules/AddonMods/tuktui_lfrdatatext.lua
+++ SanUI/Modules/AddonMods/tuktui_lfrdatatext.lua
@@ -144,7 +144,7 @@ local OnEnter = function(self)
 			local RolesString = MakeString(TankReward, HealerReward, DPSReward)
 
 			if (RolesString ~= "   ")  then
-			local numEncounters, numCompleted = GetLFGDungeonNumEncounters(i)
+			local numEncounters, numCompleted = GetLFGDungeonNumEncounters(ID)
 				numEncounters = numEncounters or 0
 				numCompleted = numCompleted or 0
 				GameTooltip:AddDoubleLine(Name .. "(" .. numCompleted .. "/" .. numEncounters .. "):", RolesString, 1, 1, 1)
