9ea68f59b935c0e7c87e74d582c968d39b4dadf6
SanUI <SanUI@users.noreply.github.com>
2020-11-23 19:31:41 +0100

    Update raidframes debuffs

--- SanUI/Modules/Unitframes/tracking.lua
+++ SanUI/Modules/Unitframes/tracking.lua
@@ -170,42 +170,52 @@ S["UnitFrames"].RaidDebuffs = {
 		[226512] = Defaults(), -- Sanguine
 		[240559] = Defaults(), -- Grievous
 		[240443] = Defaults(), -- Bursting
-		-- 8.3 Affix
-		[314531] = Defaults(), -- Tear Flesh
-		[314308] = Defaults(), -- Spirit Breaker
-		[314478] = Defaults(), -- Cascading Terror
-		[314483] = Defaults(), -- Cascading Terror
-		[314592] = Defaults(), -- Mind Flay
-		[314406] = Defaults(), -- Crippling Pestilence
-		[314411] = Defaults(), -- Lingering Doubt
-		[314565] = Defaults(), -- Defiled Ground
-		[314392] = Defaults(), -- Vile Corruption
-		-- Shadowlands
+		-- Shadowlands Affix
 		[342494] = Defaults(), -- Belligerent Boast (Prideful)
 
 	-- Shadowlands Dungeons
 		-- Halls of Atonement
 		[335338] = Defaults(), -- Ritual of Woe
 		[326891] = Defaults(), -- Anguish
-		[329321] = Defaults(), -- Jagged Swipe
+		[329321] = Defaults(), -- Jagged Swipe 1
+		[344993] = Defaults(), -- Jagged Swipe 2
 		[319603] = Defaults(), -- Curse of Stone
 		[319611] = Defaults(), -- Turned to Stone
 		[325876] = Defaults(), -- Curse of Obliteration
 		[326632] = Defaults(), -- Stony Veins
 		[323650] = Defaults(), -- Haunting Fixation
 		[326874] = Defaults(), -- Ankle Bites
+		[340446] = Defaults(), -- Mark of Envy
 		-- Mists of Tirna Scithe
 		[325027] = Defaults(), -- Bramble Burst
 		[323043] = Defaults(), -- Bloodletting
 		[322557] = Defaults(), -- Soul Split
 		[331172] = Defaults(), -- Mind Link
 		[322563] = Defaults(), -- Marked Prey
+		[322487] = Defaults(), -- Overgrowth 1
+		[322486] = Defaults(), -- Overgrowth 2
+		[328756] = Defaults(), -- Repulsive Visage
+		[325021] = Defaults(), -- Mistveil Tear
+		[321891] = Defaults(), -- Freeze Tag Fixation
+		[325224] = Defaults(), -- Anima Injection
+		[326092] = Defaults(), -- Debilitating Poison
+		[325418] = Defaults(), -- Volatile Acid
 		-- Plaguefall
 		[336258] = Defaults(), -- Solitary Prey
 		[331818] = Defaults(), -- Shadow Ambush
 		[329110] = Defaults(), -- Slime Injection
 		[325552] = Defaults(), -- Cytotoxic Slash
 		[336301] = Defaults(), -- Web Wrap
+		[322358] = Defaults(), -- Burning Strain
+		[322410] = Defaults(), -- Withering Filth
+		[328180] = Defaults(), -- Gripping Infection
+		[320542] = Defaults(), -- Wasting Blight
+		[340355] = Defaults(), -- Rapid Infection
+		[328395] = Defaults(), -- Venompiercer
+		[320512] = Defaults(), -- Corroded Claws
+		[333406] = Defaults(), -- Assassinate
+		[332397] = Defaults(), -- Shroudweb
+		[330069] = Defaults(), -- Concentrated Plague
 		-- The Necrotic Wake
 		[321821] = Defaults(), -- Disgusting Guts
 		[323365] = Defaults(), -- Clinging Darkness
@@ -216,25 +226,56 @@ S["UnitFrames"].RaidDebuffs = {
 		[320170] = Defaults(), -- Necrotic Bolt
 		[323464] = Defaults(), -- Dark Ichor
 		[323198] = Defaults(), -- Dark Exile
+		[343504] = Defaults(), -- Dark Grasp
+		[343556] = Defaults(), -- Morbid Fixation 1
+		[338606] = Defaults(), -- Morbid Fixation 2
+		[324381] = Defaults(), -- Chill Scythe
+		[320573] = Defaults(), -- Shadow Well
+		[333492] = Defaults(), -- Necrotic Ichor
+		[334748] = Defaults(), -- Drain FLuids
+		[333489] = Defaults(), -- Necrotic Breath
+		[320717] = Defaults(), -- Blood Hunger
 		-- Theater of Pain
-		[333299] = Defaults(), -- Curse of Desolation
+		[333299] = Defaults(), -- Curse of Desolation 1
+		[333301] = Defaults(), -- Curse of Desolation 2
 		[319539] = Defaults(), -- Soulless
 		[326892] = Defaults(), -- Fixate
 		[321768] = Defaults(), -- On the Hook
 		[323825] = Defaults(), -- Grasping Rift
+		[342675] = Defaults(), -- Bone Spear
+		[323831] = Defaults(), -- Death Grasp
+		[330608] = Defaults(), -- Vile Eruption
+		[330868] = Defaults(), -- Necrotic Bolt Volley
+		[323750] = Defaults(), -- Vile Gas
+		[323406] = Defaults(), -- Jagged Gash
+		[330700] = Defaults(), -- Decaying Blight
+		[319626] = Defaults(), -- Phantasmal Parasite
+		[324449] = Defaults(), -- Manifest Death
+		[341949] = Defaults(), -- Withering Blight
 		-- Sanguine Depths
 		[326827] = Defaults(), -- Dread Bindings
 		[326836] = Defaults(), -- Curse of Suppression
 		[322554] = Defaults(), -- Castigate
 		[321038] = Defaults(), -- Burden Soul
-		[328593] = Defaults(), -- Agonize (Channel)
+		[328593] = Defaults(), -- Agonize
 		[325254] = Defaults(), -- Iron Spikes
+		[335306] = Defaults(), -- Barbed Shackles
+		[322429] = Defaults(), -- Severing Slice
+		[334653] = Defaults(), -- Engorge
 		-- Spires of Ascension
 		[338729] = Defaults(), -- Charged Stomp
 		[338747] = Defaults(), -- Purifying Blast
 		[327481] = Defaults(), -- Dark Lance
 		[322818] = Defaults(), -- Lost Confidence
 		[322817] = Defaults(), -- Lingering Doubt
+		[324205] = Defaults(), -- Blinding Flash
+		[331251] = Defaults(), -- Deep Connection
+		[328331] = Defaults(), -- Forced Confession
+		[341215] = Defaults(), -- Volatile Anima
+		[323792] = Defaults(), -- Anima Field
+		[317661] = Defaults(), -- Insidious Venom
+		[330683] = Defaults(), -- Raw Anima
+		[328434] = Defaults(), -- Intimidated
 		-- De Other Side
 		[320786] = Defaults(), -- Power Overwhelming
 		[334913] = Defaults(), -- Master of Death
@@ -244,6 +285,16 @@ S["UnitFrames"].RaidDebuffs = {
 		[339978] = Defaults(), -- Pacifying Mists
 		[323692] = Defaults(), -- Arcane Vulnerability
 		[333250] = Defaults(), -- Reaver
+		[330434] = Defaults(), -- Buzz-Saw 1
+		[320144] = Defaults(), -- Buzz-Saw 2
+		[331847] = Defaults(), -- W-00F
+		[327649] = Defaults(), -- Crushed Soul
+		[331379] = Defaults(), -- Lubricate
+		[332678] = Defaults(), -- Gushing Wound
+		[322746] = Defaults(), -- Corrupted Blood
+		[323687] = Defaults(), -- Arcane Lightning
+		[323877] = Defaults(), -- Echo Finger Laser X-treme
+		[334535] = Defaults(), -- Beak Slice
 
 	-- Castle Nathria
 		-- Shriekwing
@@ -254,9 +305,9 @@ S["UnitFrames"].RaidDebuffs = {
 		-- Huntsman Altimor
 		[335304] = Defaults(), -- Sinseeker
 		[334971] = Defaults(), -- Jagged Claws
-		[335113] = Defaults(), -- Huntsman's Mark 1
+		[335111] = Defaults(), -- Huntsman's Mark 1
 		[335112] = Defaults(), -- Huntsman's Mark 2
-		[335111] = Defaults(), -- Huntsman's Mark 3
+		[335113] = Defaults(), -- Huntsman's Mark 3
 		[334945] = Defaults(), -- Bloody Thrash
 		-- Hungering Destroyer
 		[334228] = Defaults(), -- Volatile Ejection
@@ -265,7 +316,7 @@ S["UnitFrames"].RaidDebuffs = {
 		[325936] = Defaults(), -- Shared Cognition
 		[335396] = Defaults(), -- Hidden Desire
 		[324983] = Defaults(), -- Shared Suffering
-		[324982] = Defaults(), -- Shared Suffering Partner
+		[324982] = Defaults(), -- Shared Suffering (Partner)
 		[332664] = Defaults(), -- Concentrate Anima
 		[325382] = Defaults(), -- Warped Desires
 		-- Sun King's Salvation
@@ -278,8 +329,8 @@ S["UnitFrames"].RaidDebuffs = {
 		[325236] = Defaults(), -- Glyph of Destruction
 		[327414] = Defaults(), -- Possession
 		-- The Council of Blood
-		[327773] = Defaults(), -- Drain Essence 1
-		[327052] = Defaults(), -- Drain Essence 2
+		[327052] = Defaults(), -- Drain Essence 1
+		[327773] = Defaults(), -- Drain Essence 2
 		[346651] = Defaults(), -- Drain Essence Mythic
 		[328334] = Defaults(), -- Tactical Advance
 		[330848] = Defaults(), -- Wrong Moves
@@ -305,8 +356,12 @@ S["UnitFrames"].RaidDebuffs = {
 		[342698] = Defaults(), -- Volatile Anima Infection
 		-- Sire Denathrius
 		[326851] = Defaults(), -- Blood Price
-		[327798] = Defaults(), -- Night Hunter
+		[327796] = Defaults(), -- Night Hunter
 		[327992] = Defaults(), -- Desolation
 		[328276] = Defaults(), -- March of the Penitent
 		[326699] = Defaults(), -- Burden of Sin
+		[329181] = Defaults(), -- Wracking Pain
+		[335873] = Defaults(), -- Rancor
+		[329951] = Defaults(), -- Impale
+
 }
