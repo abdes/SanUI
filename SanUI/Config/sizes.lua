2bbc8ea95031bec93af20853fdf1b2587573c9a9
SanUI <SanUI@users.noreply.github.com>
2022-04-26 22:24:35 +0200

    Introduce configurable scaling for SanUI

--- SanUI/Config/sizes.lua
+++ SanUI/Config/sizes.lua
@@ -3,23 +3,36 @@ local S = unpack(addon)
 
 local Scale = S.Toolkit.Functions.Scale
 
+local function scale_entries(t, fac)
+	for k, v in pairs(t) do
+		if type(v) == "number" then
+			t[k] = Scale(fac * v)
+		elseif type(v) == "table" then
+			scale_entries(v, fac)
+		end
+	end
+end
+
+
 local sizes = {
 	scales = {
 	},
 	raidframes = {
-		width = Scale(66),
-		height = Scale(28),
-		raidicon = Scale(16),
-		readycheck = Scale(12),
-		resurrecticon = Scale(12),
-		summonindicator = Scale(28),
-		turtleicon = Scale(15),
-		raiddebuffs = Scale(15),
-		raiddebuffsicon = Scale(14),
-		notauratrackicon = Scale(6),
+		width = 66,
+		height = 28,
+		raidicon =16,
+		readycheck = 12,
+		resurrecticon = 12,
+		summonindicator = 28,
+		turtleicon = 15,
+		raiddebuffs = 15,
+		raiddebuffsicon = 14,
+		notauratrackicon = 6,
 		
 	},
 }
+
+scale_entries(sizes, 1)
 
 for i = -10, 12 do
 	sizes.scales[i] = Scale(i)
