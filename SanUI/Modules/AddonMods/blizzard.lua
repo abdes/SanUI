5b543e48b325c37c4bcab7d767cb67c6a552a438
SanUI <SanUI@users.noreply.github.com>
2022-11-19 10:58:45 +0100

    Properly disable blizzard stuff

--- SanUI/Modules/AddonMods/blizzard.lua
+++ SanUI/Modules/AddonMods/blizzard.lua
@@ -14,49 +14,25 @@ function S.disableBlizzard()
 		Boss:UnregisterAllEvents()
 		Boss.Show = Noop
 		Boss:Hide()
-
-		--Health:UnregisterAllEvents()
-		--Power:UnregisterAllEvents()
 	end
 
-	--InterfaceOptionsFrameCategoriesButton11:SetScale(0.00001)
-	--InterfaceOptionsFrameCategoriesButton11:SetAlpha(0)
-
-	CompactRaidFrameManager:SetParent(S.Hider)
-	CompactRaidFrameManager:UnregisterAllEvents()
-	CompactRaidFrameManager:Hide()
-
-	CompactRaidFrameContainer:SetParent(S.Hider)
-	CompactRaidFrameContainer:UnregisterAllEvents()
-	CompactRaidFrameContainer:Hide()
-	--CompactUnitFrameProfiles:UnregisterAllEvents()
-
-	--[[
-	for i = 1, MAX_PARTY_MEMBERS do
-		local PartyMember = _G["PartyMemberFrame" .. i]
-		local Health = _G["PartyMemberFrame" .. i .. "HealthBar"]
-		local Power = _G["PartyMemberFrame" .. i .. "ManaBar"]
-		local Pet = _G["PartyMemberFrame" .. i .."PetFrame"]
-		local PetHealth = _G["PartyMemberFrame" .. i .."PetFrame" .. "HealthBar"]
-
-		PartyMember:UnregisterAllEvents()
-		PartyMember:SetParent(S.Hider)
-		PartyMember:Hide()
-		Health:UnregisterAllEvents()
-		Power:UnregisterAllEvents()
-
-		Pet:UnregisterAllEvents()
-		Pet:SetParent(S.Hider)
-		PetHealth:UnregisterAllEvents()
-
-		HidePartyFrame()
-		ShowPartyFrame = Noop
-		HidePartyFrame = Noop
+	local PartyFrame = PartyFrame
+	
+	if PartyFrame then
+		PartyFrame:SetParent(S.Hider)
 	end
 
-	InterfaceOptionsFrameCategoriesButton9:SetHeight(0.00001)
-	InterfaceOptionsFrameCategoriesButton9:SetAlpha(0)
-	InterfaceOptionsFrameCategoriesButton10:SetHeight(0.00001)
-	InterfaceOptionsFrameCategoriesButton10:SetAlpha(0)
-	--]]
+	if CompactRaidFrameManager then
+		-- Disable Blizzard Raid Frames.
+		CompactRaidFrameManager:SetParent(S.Hider)
+		CompactRaidFrameManager:UnregisterAllEvents()
+		CompactRaidFrameManager:Hide()
+
+		CompactRaidFrameContainer:SetParent(S.Hider)
+		CompactRaidFrameContainer:UnregisterAllEvents()
+		CompactRaidFrameContainer:Hide()
+		
+		UIParent:UnregisterEvent("GROUP_ROSTER_UPDATE")
+		CompactRaidFrameManager_SetSetting("IsShown", "0")
+	end
 end
